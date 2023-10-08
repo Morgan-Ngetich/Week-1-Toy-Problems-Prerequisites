e68691c8b9f41e80346813f17c0ebf9d393c919a
Morgan-Ngetich <ngetichmorgan6@gmail.com>
2023-10-09 02:14:27 +0300

    Some comments

--- Speed-detector.js
+++ Speed-detector.js
@@ -9,20 +9,20 @@ function speedDetector() {
    // Checks if speed is a Number or is less than 0
   if (isNaN(speed) || speed < 0) {
     console.log('Invalid input. Please enter a valid number!!! Should be greater than 0');
-  } else if (speed < 70) {
-    console.log('Ok');
+  } else if (speed < 70) {   // If speed is less than 70, it dislpays "Ok."
+    console.log('Ok.');
   } else {
-    const aboveLimit = speed - 70;
-    const demeritPoints = Math.ceil(aboveLimit / 5);  // Rounds down the number to the nearest integer
+    const aboveLimit = speed - 70;   // if speed is above limit it gets the above limit through subtraction.
+    const demeritPoints = Math.ceil(aboveLimit / 5);  // Divides the aboveLimit and Rounds up the number to the nearest integer
     console.log(`Demerit points: ${demeritPoints}`);
     if (demeritPoints > 12) {
       console.log('License suspended!!!')
     }
   }
-  //Calling the function
+  
 } 
 
-let speed ;
+let speed ;   //Declaring a variable the userInput will be assigned to (line: 36)
 
 //Exit: By typing "exit" or "Quit",the program terminates
 while (true) {
@@ -34,7 +34,7 @@ while (true) {
   }
   
   speed =  parseFloat(userInput);  // converts the speed from a string to a number
-  speedDetector();
+  speedDetector();   // Calling speedDector function
 
 }
 
